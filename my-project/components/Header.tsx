4e944e3e05f3e44a07eaed338ce9c99b062d4f38
Tai <taiu.engineer@gmail.com>
2023-01-06 19:30:44 +0900

    develop header

--- my-project/components/Header.tsx
+++ my-project/components/Header.tsx
@@ -1,8 +1,31 @@
 import Image from "next/image";
+import { BellIcon, SearchIcon } from "@heroicons/react/solid";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // if the display is scrolled, set "true"
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header>
+    // <header className={`${isScrolled && "bg-[#e72e2e]"}`}>
+    <header className={`${isScrolled && "bg-[#141414]"}`}>
       <div className="flex items-center space-x-2 md:space-x-10">
         <picture>
           <img
@@ -14,13 +37,28 @@ const Header = () => {
           />
         </picture>
 
-        <ul>
-          <li>Home</li>
-          <li>TV Shows</li>
-          <li>Movies</li>
-          <li>New & Popular</li>
-          <li>My List</li>
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
         </ul>
+
+        <div className="flex items-center space-x-4 text-sm font-light">
+          <SearchIcon className="sm hidden h-6 w-6 sm:inline" />
+          <p className="hidden lg:inline">Kids</p>
+          <BellIcon className="h-6 w-6" />
+          <Link href="/account">
+            <picture>
+              <img
+                src="https://rb.gy/g1pwyx"
+                alt=""
+                className="cursor-pointer rounded"
+              />
+            </picture>
+          </Link>
+        </div>
       </div>
     </header>
   );
