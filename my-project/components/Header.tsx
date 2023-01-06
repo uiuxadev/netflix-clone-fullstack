83dbec16351dc3a166499de3d19c166d66076201
Tai <taiu.engineer@gmail.com>
2023-01-07 00:07:42 +0900

    dev banner

--- my-project/components/Header.tsx
+++ my-project/components/Header.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 import { BellIcon, SearchIcon } from "@heroicons/react/solid";
 import Link from "next/link";
 import { useEffect, useState } from "react";
