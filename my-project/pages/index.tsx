d6fa48a011d2f0195ef84f38dafd71137b60ed2d
Tai <taiu.engineer@gmail.com>
2023-01-07 11:48:43 +0900

    complete banner by setting margin

--- my-project/pages/index.tsx
+++ my-project/pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home({
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main>
+      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
         <Banner netflixOriginals={netflixOriginals} />
         <p>te</p>
         <p>te</p>
