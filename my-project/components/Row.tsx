620634bf023af9a1ba1c213a58c8a25da1f4e1d5
Tai <taiu.engineer@gmail.com>
2023-01-07 19:54:16 +0900

    edit scroll arrow

--- my-project/components/Row.tsx
+++ my-project/components/Row.tsx
@@ -1,4 +1,5 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
+import { useRef, useState } from "react";
 import { Movie } from "../typings";
 import Thumbnail from "./Thumbnail";
 
@@ -9,21 +10,37 @@ interface Props {
   movies: Movie[];
 }
 
-const Row = ({ title, movies }) => {
+const Row = ({ title, movies }: Props) => {
+  const rowRef = useRef<HTMLDivElement>(null);
+  const [isMoved, setisMoved] = useState(false)
+
+  const handleClick = (direction: string) => {
+    setIsMoved(true)
+
+    if(rowRef.current) {
+        const {scrollLeft} = rowRef.current
+    }
+  }
+
   return (
     <div className="h-40 space-y-0.5 md:space-y-2">
       <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">
         {title}
       </h2>
       <div className="group relative md:-ml-2">
-        <ChevronLeftIcon className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-1 transition hover:scale-125 group-hover:opacity-100" />
+        <ChevronLeftIcon className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-1 transition hover:scale-125 group-hover:opacity-100" 
+        onClick={() => handleClick("left")} />
 
-        <div className="flex scrollbar-hide items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2">
+        <div
+          ref={rowRef}
+          className="flex scrollbar-hide items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2"
+        >
           {movies.map((movie) => (
             <Thumbnail key={movie.id} movie={movie} />
           ))}
         </div>
-        <ChevronRightIcon className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-1 transition hover:scale-125 group-hover:opacity-100" />
+        <ChevronRightIcon className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-1 transition hover:scale-125 group-hover:opacity-100" 
+        onClick={() => handleClick("right")} />
       </div>
     </div>
   );
