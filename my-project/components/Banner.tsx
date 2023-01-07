27bc01ad72b86bf8a016608a6f9a3d1e6523b0cd
Tai <taiu.engineer@gmail.com>
2023-01-07 22:42:34 +0900

    adjust title and description size

--- my-project/components/Banner.tsx
+++ my-project/components/Banner.tsx
@@ -35,10 +35,10 @@ const Banner = ({ netflixOriginals }: Props) => {
         />
       </div>
 
-      <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
+      <h1 className="text-2xl font-bold md:text-4xl lg:text-6xl lg:max-w-xl">
         {movie?.title || movie?.name || movie?.original_name}
       </h1>
-      <p className="max-w-xs max-h-xs text-xs text-shadow-md line-clamp-4 md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+      <p className="max-w-xs max-h-xs text-xs text-shadow-md line-clamp-4 md:max-w-lg md:text-lg lg:max-w-xl lg:text-2xl">
         {movie?.overview}
       </p>
 
